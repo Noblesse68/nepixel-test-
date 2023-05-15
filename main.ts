c99ae222da546e118a76a1c7de40fc30d045f36d
Noblesse68 <132678684+Noblesse68@users.noreply.github.com>
2023-05-15 10:10:22 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+strip.setBrightness(100)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+strip.show()
 basic.forever(function () {
-	
+    basic.pause(100)
+    strip.rotate(1)
+    strip.show()
 })
